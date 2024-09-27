6f1915f489c3d04523b5ae14283eb9cb8b4d249b
cbharadwaj <bharadwaj.chitrapu@gmail.com>
2024-09-27 11:18:56 +0530

    commiting to Development Branch

--- src/test100/testClass.java
+++ src/test100/testClass.java
@@ -3,8 +3,7 @@ package test100;
 public class testClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("adding from deleteWorkSpace");
 	}
 
 }
